62042bac17edeb0f6292410b18338b041cd08238
Deependra Kumar Rout <53940680+Deependrakumarrout@users.noreply.github.com>
2020-09-20 22:10:22 +0530

    Update A_Hello_world.java

--- A_Hello_world.java
+++ A_Hello_world.java
@@ -3,19 +3,10 @@ import java.util.Random;
 public class A_Hello_world {
 
     public static void main(String[] args) {
-        //I have used here single line comment with frontward slash.
+        //I have used here single line comment with forward slash.
         //So that my program can be easily readable.
 
         System.out.println("Hello dear java 5.6");
 
-
-        // This is the square of a number:
-        /*
-        int d=1;
-        for(int i=0;i<3;i++){
-            d=d*2;
-        }
-        System.out.print(d+" ");
-         */
     }
 }
